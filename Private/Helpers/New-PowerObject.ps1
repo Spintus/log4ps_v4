7a9e60df9fef59800eb2c427308e7f4cf3de3056
Spintus <marcumwill@gmail.com>
2021-09-17 16:25:38 -0600

    Add files via upload

---
+++ Private/Helpers/New-PowerObject.ps1
@@ -0,0 +1,163 @@
+﻿function New-PowerObject
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName,
+            position = 0
+        )]
+        [type]
+        $TypeName
+    )
+
+    dynamicParam
+    {
+        function New-ParamAttribute
+        {
+            param
+            (
+                [Parameter(
+                    Mandatory = $true,
+                    ValueFromPipeLine = $false,
+                    ValueFromPipelineByPropertyName = $true,
+                    position = 0
+                )]
+                [string]
+                $ParameterSetName,
+
+                [Parameter(
+                    Mandatory = $true,
+                    ValueFromPipeLine = $false,
+                    ValueFromPipelineByPropertyName = $true,
+                    position = 1
+                )]
+                [int]
+                $ParamPosition,
+
+                [Parameter(
+                    Mandatory = $false,
+                    ValueFromPipeLine = $false,
+                    ValueFromPipelineByPropertyName = $true,
+                    position = 2
+                )]
+                [switch]
+                $DontShow,
+
+                [Parameter(
+                    Mandatory = $false,
+                    ValueFromPipeLine = $false,
+                    ValueFromPipelineByPropertyName = $true,
+                    position = 3
+                )]
+                [bool]
+                $Mandatory = $true
+            )
+
+            $newAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $newAttribute.Mandatory = $Mandatory
+            if ($DontShow) {$newAttribute.DontShow = $false}
+            $newAttribute.Position = $ParamPosition
+            $newAttribute.ParameterSetName = $ParameterSetName
+
+            return $newAttribute
+        }
+
+        $paramRuntimeCollection = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+
+        $type = ($TypeName -as [Type])
+
+        foreach ($constructorDefinition in $type.GetConstructors().Where({
+                if ($baseObject = $_.GetParameters().psobject.BaseObject)
+                {
+                    if ($baseObject.name -match 'ParameterType')
+                    {
+                        ($_.GetParameters().ParameterType.name -join '') -notmatch '\*'
+                    }
+                }
+            })
+        )
+        {
+            # Remove constructors using pointers.
+            $paramListForParamSet = @()
+
+            $paramsInCtor = $constructorDefinition.GetParameters()
+            $constructorIndex = $paramsInCtor.Name -join '.'
+
+            foreach ($paramDefinition in $paramsInCtor)
+            {
+                $ParamPosition = 1
+
+                if ($paramRuntimeCollection.Keys -notcontains $paramDefinition.Name)
+                {
+                    $newRuntimeDefinedParameter = New-Object System.Management.Automation.RuntimeDefinedParameter
+                    $newRuntimeDefinedParameter.Name = $ParamDefinition.Name
+                    $newRuntimeDefinedParameter.ParameterType = $ParamDefinition.ParameterType
+
+                    $newRuntimeDefinedParameter.Attributes.Add((New-ParamAttribute -ParameterSetName "ctor_$constructorIndex" -ParamPosition $ParamPosition))
+
+                    if ($paramDefinition.HasDefaultValue)
+                    {
+                        $newRuntimeDefinedParameter.Value = $paramDefinition.DefaultValue
+                    }
+
+                    $paramRuntimeCollection.Add($newRuntimeDefinedParameter.Name, $newRuntimeDefinedParameter)
+                }
+                else
+                {
+                    "$($paramDefinition.Name) already present in Attribute collection" | Microsoft.PowerShell.Utility\Write-Debug
+                    "Adding Attribute definition for ParameterSet $($constructorIndex) for Parameter $($paramDefinition.Name)" | Microsoft.PowerShell.Utility\Write-Debug
+                    $paramRuntimeCollection[$paramDefinition.Name].Attributes.Add((New-ParamAttribute -ParameterSetName "ctor_$constructorIndex" -ParamPosition $ParamPosition))
+                }
+
+                $paramListForParamSet += $paramDefinition.Name
+            }
+
+            $ParamPosition++
+        }
+
+        $writeableProperties = $type.GetProperties().Where({$_.CanWrite -and $_.Name -notin $paramRuntimeCollection.Keys})
+
+        foreach ($propertyArgument in $writeableProperties)
+        {
+            $newRuntimeDefinedParameter = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter
+            $newRuntimeDefinedParameter.Name = $propertyArgument.Name
+            $newRuntimeDefinedParameter.ParameterType = $propertyArgument.PropertyType
+
+            $newRuntimeDefinedParameter.Attributes.Add((New-ParamAttribute -ParameterSetName '__AllParameterSets' -paramPosition -2147483648 -mandatory $false))
+
+            $paramRuntimeCollection.Add($newRuntimeDefinedParameter.Name, $newRuntimeDefinedParameter)
+        }
+
+        # Return RuntimeDefinedParameterDictionary. Now accessible in $PSBoundParameters of parent function.
+        $paramRuntimeCollection
+    }
+
+    process
+    {
+        $constructorParameterNames = @()
+        $parameters = @()
+
+        if ($PSCmdlet.ParameterSetName -match '^ctor_')
+        {
+            $constructorParameterNames = $PSCmdlet.ParameterSetName -replace '^ctor_' -split '\.'
+            [void] $constructorParameterNames.ForEach({$parameters += $PSBoundParameters[$_]; $PSBoundParameters.Remove($_)})#########################
+        }
+
+        $instanceOfObject = New-Object -TypeName $type.ToString() -ArgumentList $parameters
+
+        $setProperties = $PSBoundParameters.Keys.Where({
+                $_ -ne 'typeName' -and
+                $_ -notin [Management.Automation.PSCmdlet]::CommonParameters -and
+                $_ -notin [Management.Automation.PSCmdlet]::OptionalCommonParameters
+            })
+
+        foreach ($paramPropKey in $setProperties)
+        {
+            $instanceOfObject.($paramPropKey) = $PSBoundParameters.($paramPropKey)
+        }
+
+        $instanceOfObject
+    }
+}
---
+++ Private/Helpers/Receive-ModuleParameter.ps1
@@ -0,0 +1,87 @@
+﻿function Receive-ModuleParameter
+{
+    param
+    (
+        $ModuleParams = $script:ModuleParams
+    )
+
+    if ($ModuleParams.count -eq 0)
+    {
+        # If no arguments, look for config file based on $PSScriptRoot and fileName.
+        $callStack = Get-PSCallStack
+        $callerFile = if ($callStack[$callstack.Count - 1].ScriptName)
+        {
+            $callStack[$callstack.Count - 1].ScriptName
+        }
+        else
+        {
+            $callStack[$callstack.Count - 2].ScriptName
+        }
+
+        try
+        {
+            [string] $callerFileRoot = Split-Path $callerFile
+        }
+        catch
+        {
+            Microsoft.PowerShell.Utility\Write-Warning $_
+        }
+
+        if ($callerFile -and
+            (
+                # Most of these are unneccesary. Remove if causes issues or decide on standard.
+                ($CallerFileName = Resolve-Path "$callerFileRoot\logging_config.xml" -ea Ignore) -or
+                ($CallerFileName = Resolve-Path "$callerFileRoot\Dependencies\$callerFile.config" -ea Ignore) -or
+                ($CallerFileName = Resolve-Path "$callerFileRoot\Dependencies\${callerFile}_config.xml" -ea Ignore) -or
+                ($CallerFileName = Resolve-Path "$callerFileRoot\Dependencies\$callerFile.config.xml" -ea Ignore) -or
+                ($CallerFileName = Resolve-Path "$callerFileRoot\Dependencies\logging_config.xml" -ea Ignore)
+            )
+        )
+        {
+            $script:configFile = $CallerFileName.Path
+        }
+        else
+        {
+            Microsoft.PowerShell.Utility\Write-Verbose 'No configuration file found, and No arguments sent. Loading hardcoded defaults'
+            $script:configFile = $null
+        }
+    }
+    elseif ($ModuleParams.count -eq 1)
+    {
+        # If one argument, check if it's a hashtable.
+        if ($ModuleParams[0] -as [hashtable])
+        {
+            # If hashtable, extract valid params for splatting to the Set-<moduleName>ModuleConfig function.
+            $moduleName = $MyInvocation.MyCommand.ModuleName
+            $ConfigCommand = Get-Command "Set-$($moduleName)ModuleConfig"
+
+            $script:ParamsForSetModuleConfig = @{}
+
+            foreach ($moduleParamKey in $ModuleParams.keys)
+            {
+                # Extract allowed parameters from Module Arguments.
+                if ($moduleParamKey -in $ConfigCommand.Parameters.Keys)
+                {
+                    $script:ParamsForSetModuleConfig.add($moduleParamKey, $ModuleParams.($moduleParamKey))
+                }
+                else
+                {
+                    Microsoft.PowerShell.Utility\Write-Warning -Message "Parameter $param was found but not allowed for Set-$($moduleName)ModuleConfig"
+                }
+            }
+
+            # Call to config need to happen at the end of the psm1 file so that all functions are defined.
+        }
+        else
+        {
+            # Only one argument but not a Hashtable, maybe it's a configuration file.
+            if ($null -ne $ModuleParams[0] -and
+                $ModuleParams[0] -ne [string]::Empty -and
+                ($configFileName = Resolve-Path $ModuleParams[0])
+            )
+            {
+                $script:configFile = $configFileName.Path
+            }
+        }
+    }
+}
